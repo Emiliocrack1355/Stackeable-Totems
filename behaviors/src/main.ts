4b7e48b9cdf21687fd15001aa18cc0c145f23a98
Emi :3 <94089705+Emiliocrack1355@users.noreply.github.com>
2026-08-31 22:22:48 -0600

    [UPDATE] Nada relevante xd
    Solo se actualiza las rutas de las clases

--- behaviors/src/main.ts
+++ behaviors/src/main.ts
@@ -1,3 +1,16 @@
-import './worldEvents/worldInit';
+import "./totemLogic/new-customTotems";
+import "./worldEvents/worldInit";
 
-import './totemLogic/totemMetods';
+/*
+- Data 0 => Vanila Totem
+- Data 1 => TNT Totem
+- Data 2 => Fly Totem
+- Data 3 => Ender Totem
+- Data 4 => Void Totem
+- Data 5 => Tp Totem
+- Data 6 => Mini Totem
+*/
+
+//  cd c:users/Brian/Desktop/Proyectos/dev
+//  npm run watch
+//
